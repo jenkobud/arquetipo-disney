f02b5859f07c3eb01d32ba3dc202ac90e9ea7f88
variniaquisbert <variniaminerva.quisbertsoria@bbva.com>
2022-05-29 12:43:01 -0300

    add genre files

--- src/main/java/com/alkemy/disney/service/GenreService.java
+++ src/main/java/com/alkemy/disney/service/GenreService.java
@@ -1,4 +1,25 @@
 package com.alkemy.disney.service;
 
-public class GenreService {
+import com.alkemy.disney.entity.Genre;
+import com.alkemy.disney.repository.GenreRepository;
+import com.alkemy.disney.service.impl.GenreServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+/*
+ * @author VariniaQuisbert
+ * @version 1.0 27-05-22
+ * This interface contains business logic methods for genres
+ */
+@Service
+public class GenreService implements GenreServiceImpl {
+    @Autowired
+    private GenreRepository data;
+
+    @Override
+    public int save(Genre genre) {
+        int res = 0;
+        Genre g = data.save(genre);
+        if (!g.equals(null)) {res=1;}
+        return res;
+    }
 }
--- src/main/java/com/alkemy/disney/service/impl/GenreServiceImpl.java
+++ src/main/java/com/alkemy/disney/service/impl/GenreServiceImpl.java
@@ -2,6 +2,6 @@ package com.alkemy.disney.service.impl;
 
 import com.alkemy.disney.entity.Genre;
 
-public interface GenreServiceInterface {
+public interface GenreServiceImpl {
     public int save(Genre genre);
 }
