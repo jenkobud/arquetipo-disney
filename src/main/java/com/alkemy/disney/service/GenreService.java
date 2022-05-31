9589688f63d8fbbeec5de1d7abb55843a6384243
variniaquisbert <variniaminerva.quisbertsoria@bbva.com>
2022-05-31 16:33:40 -0300

    modified GenreService | delete @Service annotation

--- src/main/java/com/alkemy/disney/service/GenreService.java
+++ src/main/java/com/alkemy/disney/service/GenreService.java
@@ -1,16 +1,21 @@
 package com.alkemy.disney.service;
 
+import com.alkemy.disney.dto.GenreDto;
 import com.alkemy.disney.entity.Genre;
 import com.alkemy.disney.repository.GenreRepository;
 import com.alkemy.disney.service.impl.GenreServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /*
  * @author VariniaQuisbert
  * @version 1.0 27-05-22
  * This interface contains business logic methods for genres
  */
-@Service
+
 public interface GenreService  {
-    Genre createGenre(Genre genre);
+    public GenreDto createGenre(Genre genre);
+    public List<GenreDto> getGenres();
 }
