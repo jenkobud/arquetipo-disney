ac73bdcb0cc9419d5665697a2072129ec7bfb13a
variniaquisbert <variniaminerva.quisbertsoria@bbva.com>
2022-05-31 16:30:12 -0300

    add GenreController | getMethod & postMethod

--- src/main/java/com/alkemy/disney/controller/GenreController.java
+++ src/main/java/com/alkemy/disney/controller/GenreController.java
@@ -2,27 +2,32 @@ package com.alkemy.disney.controller;
 
 import com.alkemy.disney.dto.GenreDto;
 import com.alkemy.disney.entity.Genre;
-import com.alkemy.disney.mapper.GenreMapper;
-import com.alkemy.disney.repository.GenreRepository;
 import com.alkemy.disney.service.GenreService;
 import com.alkemy.disney.service.impl.GenreServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/genre")
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+@RequestMapping("/genres")
 public class GenreController {
     @Autowired
-    private GenreServiceImpl genreService;
+    private GenreService genreService;
 
-    @Autowired
-    private GenreMapper genreMapper;
+    @GetMapping
+    @ResponseBody
+    public List<GenreDto> getGenres(){
+        return genreService.getGenres();
+    }
 
     @PostMapping
-    public ResponseEntity<GenreDto> createGenre(@RequestBody GenreDto genreDto){
-        Genre genre = genreService.createGenre(genreMapper.createEntity(genreDto));
-        return new ResponseEntity<>(genreMapper.createDTO(genre), HttpStatus.OK);
+    public ResponseEntity<GenreDto> createGenre(@Valid @RequestBody Genre genre){
+        GenreDto genreDto = genreService.createGenre(genre);
+        return new ResponseEntity<>(genreDto, HttpStatus.OK);
     }
 }
