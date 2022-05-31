4fe70d11a749dab7b60bdfb01ae085515caf57a9
variniaquisbert <variniaminerva.quisbertsoria@bbva.com>
2022-05-31 16:30:39 -0300

    add GenreDto with validations°

--- src/main/java/com/alkemy/disney/dto/GenreDto.java
+++ src/main/java/com/alkemy/disney/dto/GenreDto.java
@@ -1,15 +1,26 @@
 package com.alkemy.disney.dto;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
-@Getter
-@Setter
 @Data
 public class GenreDto implements Serializable {
+    @NotNull
     private final Long id;
+    @NotBlank(message = "This field can't be null")
     private final String name;
+    @NotBlank(message = "This field can't be null")
     private final String imgUrl;
+
+    public GenreDto(Long id, String name, String imgUrl) {
+        this.id = id;
+        this.name = name;
+        this.imgUrl = imgUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
