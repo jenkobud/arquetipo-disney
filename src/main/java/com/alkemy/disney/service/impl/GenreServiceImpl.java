36e3de50fd03e9163927d88eadadc13cb6b50371
variniaquisbert <variniaminerva.quisbertsoria@bbva.com>
2022-05-31 16:34:19 -0300

    Modified GenreServiceImpl | add @Service annotation°

--- src/main/java/com/alkemy/disney/service/impl/GenreServiceImpl.java
+++ src/main/java/com/alkemy/disney/service/impl/GenreServiceImpl.java
@@ -1,17 +1,36 @@
 package com.alkemy.disney.service.impl;
 
+import com.alkemy.disney.dto.GenreDto;
 import com.alkemy.disney.entity.Genre;
+import com.alkemy.disney.mapper.GenreMapper;
 import com.alkemy.disney.repository.GenreRepository;
 import com.alkemy.disney.service.GenreService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+@Service
 public class GenreServiceImpl implements GenreService {
 
     @Autowired
     private GenreRepository genreRepository;
 
+    @Autowired
+    private GenreMapper genreMapper;
+
     @Override
-    public Genre createGenre(Genre genre) {
-        return genreRepository.save(genre);
+    public GenreDto createGenre(Genre genre) {
+        GenreDto genreDto = genreMapper.createDTO(genre);
+        genreRepository.save(genreMapper.createEntity(genreDto));
+        return genreDto;
+    }
+
+    @Override
+    public List<GenreDto> getGenres() {
+        List<Genre> genres = genreRepository.findAll();
+        List<GenreDto> genresDto = new ArrayList<GenreDto>();
+        genres.forEach(genre -> genresDto.add(genreMapper.createDTO(genre)));
+        return genresDto;
     }
 }
