2f07f25e94a0bdd6202ee41e6cae1fefb9b00ccc
variniaquisbert <variniaminerva.quisbertsoria@bbva.com>
2022-05-31 18:20:38 -0300

    modified filmsService | change return value

--- src/main/java/com/alkemy/disney/service/FilmsService.java
+++ src/main/java/com/alkemy/disney/service/FilmsService.java
@@ -1,13 +1,14 @@
 package com.alkemy.disney.service;
 
+import com.alkemy.disney.dto.FilmBasicDto;
+import com.alkemy.disney.dto.FilmDto;
 import com.alkemy.disney.entity.Film;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface FilmsService {
-    Film createFilm(Film film);
-    List<Film> getFilms();
-    Film updateFilm(Film film);
+    FilmDto createFilm(Film film);
+    FilmBasicDto updateFilm(Film film);
     void deleteFilm(Long id);
 }
