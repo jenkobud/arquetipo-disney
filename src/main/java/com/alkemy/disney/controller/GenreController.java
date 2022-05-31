b23b2b2c34d6c4cd24ebcb366bfd15fc6fdaabb6
variniaquisbert <variniaminerva.quisbertsoria@bbva.com>
2022-05-31 00:33:57 -0300

    modified genreMapper & implement it

--- src/main/java/com/alkemy/disney/controller/GenreController.java
+++ src/main/java/com/alkemy/disney/controller/GenreController.java
@@ -1,10 +1,14 @@
 package com.alkemy.disney.controller;
 
+import com.alkemy.disney.dto.GenreDto;
 import com.alkemy.disney.entity.Genre;
+import com.alkemy.disney.mapper.GenreMapper;
 import com.alkemy.disney.repository.GenreRepository;
 import com.alkemy.disney.service.GenreService;
 import com.alkemy.disney.service.impl.GenreServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,8 +17,12 @@ public class GenreController {
     @Autowired
     private GenreServiceImpl genreService;
 
+    @Autowired
+    private GenreMapper genreMapper;
+
     @PostMapping
-    public Genre createGenre(@RequestBody Genre genre){
-        return this.genreService.createGenre(genre);
+    public ResponseEntity<GenreDto> createGenre(@RequestBody GenreDto genreDto){
+        Genre genre = genreService.createGenre(genreMapper.createEntity(genreDto));
+        return new ResponseEntity<>(genreMapper.createDTO(genre), HttpStatus.OK);
     }
 }
--- src/main/java/com/alkemy/disney/mapper/GenreMapper.java
+++ src/main/java/com/alkemy/disney/mapper/GenreMapper.java
@@ -1,15 +1,26 @@
 package com.alkemy.disney.mapper;
 
 import com.alkemy.disney.dto.GenreDto;
+import com.alkemy.disney.entity.Film;
 import com.alkemy.disney.entity.Genre;
 import com.alkemy.disney.exception.NotImplementedException;
+import com.alkemy.disney.repository.GenreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
 
 public class GenreMapper {
+    @Autowired
+    private GenreRepository genreRepository;
+
     public GenreDto createDTO(Genre genre) {
         return new GenreDto(genre.getId(), genre.getName(), genre.getImgUrl());
     }
 
     public Genre createEntity(GenreDto genre) {
-        throw new NotImplementedException();
+
+        Optional<Genre> genreOptional = genreRepository.findById(GenreDto.getId());
+        if(!genreOptional.isPresent()) throw new NullPointerException(); //TODO Change for a more specific exception
+        return genreOptional.get();
     }
 }
