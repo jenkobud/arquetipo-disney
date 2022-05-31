31565b9908b283680ca32e14b4d0638453b6b1e6
variniaquisbert <variniaminerva.quisbertsoria@bbva.com>
2022-05-31 17:41:23 -0300

    restore restErrorHandler content

--- src/main/java/com/alkemy/disney/controller/RestErrorHandler.java
+++ src/main/java/com/alkemy/disney/controller/RestErrorHandler.java
@@ -12,26 +12,6 @@ import javax.servlet.http.HttpServletRequest;
 import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.NoSuchElementException;
 
-@Order(Ordered.HIGHEST_PRECEDENCE)
-@ControllerAdvice()
-public class RestErrorHandler extends ResponseEntityExceptionHandler {
-
-    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
-    public ResponseEntity<Object> handleSqlIntegrityException(HttpServletRequest req, SQLIntegrityConstraintViolationException ex){
-
-        String error = "Unable to submit post: " + ex.getMessage();
-        return buildResponseEntity(new ErrorResponse(HttpStatus.BAD_REQUEST, error));
-    }
-
-    @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<Object> EmptyResultDataAccessException(HttpServletRequest req,NoSuchElementException ex){
-        ErrorResponse response = new ErrorResponse(HttpStatus.NOT_FOUND);
-        response.setMessage("The row for address is not existent: " + req.getRequestURI());
-        return buildResponseEntity(response);
-    }
-
-    private ResponseEntity<Object> buildResponseEntity(ErrorResponse errorResponse){
-        return new ResponseEntity<Object>(errorResponse, errorResponse.getStatus());
-    }
+public class RestErrorHandler {
 
 }
