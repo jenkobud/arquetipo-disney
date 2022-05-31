2fc98d68e24d1984d8a01e10746d33f79e346e42
jenkobud <jenkobudne@gmail.com>
2022-05-31 19:40:04 -0300

    Uploaded some postman_collection

--- src/main/java/com/alkemy/disney/mapper/GenreMapper.java
+++ src/main/java/com/alkemy/disney/mapper/GenreMapper.java
@@ -2,6 +2,7 @@ package com.alkemy.disney.mapper;
 
 import com.alkemy.disney.dto.GenreDto;
 import com.alkemy.disney.entity.Genre;
+import com.alkemy.disney.exception.NotImplementedException;
 import com.alkemy.disney.repository.GenreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -19,17 +20,9 @@ public class GenreMapper {
         return new GenreDto(genre.getId(), genre.getName(), genre.getImgUrl());
     }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     public Genre createEntity(GenreDto genreDto) {
         Optional<Genre> genreOptional = genreRepository.findById(genreDto.getId());
         if(!genreOptional.isPresent()) throw new NullPointerException();
         return genreOptional.get();
     }
-=======
-    public Genre createEntity(GenreDto genre) {throw new NotImplementedException();}
->>>>>>> 5da8210c3567c2beeca91c73e4e9973db360e326
-=======
-    public Genre createEntity(GenreDto genre) {throw new NotImplementedException();}
->>>>>>> 9109e66afecc3db08e4638624fe191c4917a4efb
 }
