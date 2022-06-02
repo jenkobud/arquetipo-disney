1e8a482ed71afcade886bf57f3aa3f8bd5423486
variniaquisbert <variniaminerva.quisbertsoria@bbva.com>
2022-06-02 14:03:29 -0300

    modified file to add an endpoint to addPersonage

--- src/main/java/com/alkemy/disney/controller/FilmController.java
+++ src/main/java/com/alkemy/disney/controller/FilmController.java
@@ -49,6 +49,11 @@ public class FilmController {
     public ResponseEntity<FilmDto> getFilmById(@Valid @PathVariable("id") Long id){
         FilmDto filmDto = filmsService.getFilmById(id);
         return ResponseEntity.status(HttpStatus.OK).body(filmDto);
+    }
 
+    @PostMapping(value = "/{idFilm}/characters/{idCharacter}")
+    public ResponseEntity<FilmDto> createPersonageToFilm(@PathVariable("idFilm") long idFilm, @PathVariable("idCharacter") long idCharacter){
+        FilmDto filmDto = filmsService.addPersonage(idFilm, idCharacter);
+        return new ResponseEntity<>(filmDto, HttpStatus.CREATED);
     }
 }
